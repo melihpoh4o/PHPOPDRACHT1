68c03f4dfba43b388f92917d2aaa0cc63e9d7120
Melih <mredzhebov@roc-dev.com>
2019-01-25 11:39:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Melih
+ * Date: 25-1-2019
+ * Time: 08:19
+ */
+echo "Hello World";
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Melih
+ * Date: 25-1-2019
+ * Time: 10:23
+ */
+
+phpinfo();
